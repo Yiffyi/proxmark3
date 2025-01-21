aa0133b9d5ed28de7286e553f194e141014b60d4
Yiffyi Jia <git@yif.fyi>
2025-01-21 05:29:07 +0000

    add: skeleton for FMCOS operations

---
+++ client/src/cmdhf14afmcos.h
@@ -0,0 +1,13 @@
+
+#ifndef CMDHF14AFMCOS_H__
+#define CMDHF14AFMCOS_H__
+
+#include "common.h"
+#include "pm3_cmd.h" //hf14a_config
+#include "mifare.h" // structs
+
+int CmdHF14AFMCOS(const char *Cmd);
+int CmdHF14AFMCOSSim(const char *Cmd);
+int CmdHF14AFMCOSInfo(const char *Cmd);
+int SelectAndRead(const char sFileName[], const char sSelectCmd[], const char sReadCmd[], bool activateField, bool keepFieldOn, uint8_t *response, size_t szResponseMax, int *szResponse);
+#endif
