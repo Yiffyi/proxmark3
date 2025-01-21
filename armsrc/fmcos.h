8918fb17c1daa59522339b1050c7c88b85ee2818
Yiffyi Jia <git@yif.fyi>
2025-01-21 08:00:17 +0000

    add: pass arm build

--- armsrc/fmcos.h
+++ armsrc/fmcos.h
@@ -6,6 +6,7 @@
 #include "pm3_cmd.h"
 #include "crc16.h" // compute_crc
 
-void SimulateFMCOSTag(uint8_t tagType, uint8_t *uid,
+void GenerateFMCOSResponse(uint8_t *receivedCmd, int receivedCmdLen, tag_response_info_t *dynamic_response_info);
+void SimulateFMCOSTag(uint8_t *uid,
                       uint8_t *iRATs, size_t irats_len);
 #endif /* __FMCOS_H */
