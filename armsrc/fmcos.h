448398407f934f629b391b1b5bc7898a44fb1929
Yiffyi Jia <git@yif.fyi>
2025-01-21 20:12:41 +0800

    fix: header declaration

--- armsrc/fmcos.h
+++ armsrc/fmcos.h
@@ -6,7 +6,8 @@
 #include "pm3_cmd.h"
 #include "crc16.h" // compute_crc
 
-void GenerateFMCOSResponse(uint8_t *receivedCmd, int receivedCmdLen, tag_response_info_t *dynamic_response_info);
+void GenerateFMCOSResponse(uint8_t *receivedCmd, int receivedCmdLen, tag_response_info_t *resp, int headerOffset);
+void PrepareDynamicResponse(uint8_t *receivedCmd, int receivedCmdLen, tag_response_info_t *resp);
 void SimulateFMCOSTag(uint8_t *uid,
                       uint8_t *iRATs, size_t irats_len);
 #endif /* __FMCOS_H */
